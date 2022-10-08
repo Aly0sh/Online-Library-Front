53b965fdf8065c7bb1a480f8f829cf3319dac0e1
Алакен Кубатбеков <kubatbekovalaken@gmail.com>
2022-10-08 15:41:53 +0600

    Navbar without adap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="firstNav">
+    <div class="logo">
+      <img src="./assets/images/B_O_Black.svg" alt=""/>
+    </div>
+    <ul class="links">
+      <li><a href="" class="lexend link">BOOKS</a></li>
+      <li><a href="" class="lexend link">BOOKS</a></li>
+      <li><a href="" class="lexend link">BOOKS</a></li>
+    </ul>
+    <ul class="icons">
+      <li><img src="./assets/images/cart-black.svg" alt=""></li>
+      <li><img src="./assets/images/icon_geoloc.svg" alt=""></li>
+      <li><img src="./assets/images/icon_login.svg" alt=""></li>
+    </ul>
+  </nav> 
+  <nav class="secondNav">
+    
+  </nav> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="css" src="./assets/css/style.css"></style>
